56521f34d296c28949f9893e2887ccd6b1cc27d9
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-10 13:47:56 +0100

    Agrego imágenes y nuevas distribuciones

---
+++ index.js
@@ -0,0 +1,10 @@
+let elemento = {
+    a:function (){
+    console.log("Starting ...")},
+    b: function (){
+        console.log("Starting ... 2")
+    }
+}
+/*function main(){
+    console.log("Starting ...")}*/
+window.addEventListener("load",elemento);
